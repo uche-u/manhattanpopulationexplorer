f7f8a319389c20443800e57f50ce26d1962d6c8a
uche-u <70341288+uche-u@users.noreply.github.com>
2024-06-25 12:26:52 -0400

    Update manpop.js

--- scripts/manpop.js
+++ scripts/manpop.js
@@ -1,5 +1,5 @@
 // Public Token
-mapboxgl.accessToken = "pk.eyJ1IjoidXV3YW5ha2EiLCJhIjoiY2x4dDg1dmJyMDIxejJwcHVjMzExZ3RsMiJ9.6ztzCnRVo23Gc7jT7EwFNg";
+mapboxgl.accessToken = "pk.eyJ1IjoidXV3YW5ha2EiLCJhIjoiY2x4dDhiZjJqMW1kbjJscHdyajJibHlzeCJ9.aE0kwDilSf9Q666Y8nvV4g";
 
 // Global vars
 var vizControl = d3.select("#mode-viz");
